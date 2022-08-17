70d4d4945254e110ba0ad4ad2797bb86d57da9db
Bauluccy <fifoluciano@hotmail.com.br>
2022-08-17 10:49:14 -0300

    Alpha 0.5

--- teste-GoogleAPI/Form1.cs
+++ teste-GoogleAPI/Form1.cs
@@ -10,7 +10,8 @@ using System.Windows.Forms;
 using FireSharp.Config;
 using FireSharp.Interfaces;
 using FireSharp.Response;
-
+using Firebase.Storage;
+using System.IO;
 
 namespace teste_GoogleAPI
 {
@@ -21,29 +22,49 @@ namespace teste_GoogleAPI
         {
             InitializeComponent();
         }
-        IFirebaseConfig client;
+        
         private void Form1_Load(object sender, EventArgs e)
         {
-            client = new FireSharp.FirebaseClient(config);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             openFileDialog1.Title = "Selecione o arquivo alvo";
-            openFileDialog1.ShowDialog();
 
             if(openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 textBox1.Text = openFileDialog1.FileName;
             }
 
-
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
+            var stream = File.Open(textBox1.Text, FileMode.Open);
 
+            FileInfo fileinfo = new FileInfo(openFileDialog1.FileName);
+
+            var task = new FirebaseStorage("teste-api-6d2b0.appspot.com")
+                .Child(fileinfo.Name)
+                .PutAsync(stream);
+
+            task.Progress.ProgressChanged += (s, f) => Console.WriteLine($"Progress: {f.Percentage} %");
+
+            MessageBox.Show("Sucesso ao enviar o arquivo!!!");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!comboBox1.SelectedItem.Equals("Manualmente"))
+            {
+                label4.Enabled = true;
+                dateTimePicker1.Enabled = true;
+            }
+            else
+            {
+                label4.Enabled = false;
+                dateTimePicker1.Enabled = false;
+            }
         }
     }
 }
